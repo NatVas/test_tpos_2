c3ae2dee4763a6601a1c4f0ff6fa64aae7cb10bc
NatVas <vas.nat.al@yandex.ru>
2019-12-16 00:35:02 +0300

    Update my_moker.py

--- my_moker.py
+++ my_moker.py
@@ -6,7 +6,7 @@ list_of_dir = []
 
 
 def init(directory):
-    btrfsutil.create_subvolume(directory, 0, NULL, NULL);
+    btrfsutil.create_subvolume(directory,  async_=True);
     list_of_dir.append(directory)
     return len(list_of_dir-1)
 
