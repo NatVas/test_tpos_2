8cb2de4d21af409f1d0846b1b493020eeb589c9b
NatVas <vas.nat.al@yandex.ru>
2019-12-16 02:54:06 +0300

    Update my_moker.py

--- my_moker.py
+++ my_moker.py
@@ -39,6 +39,7 @@ def init(directory):
             mocker_init(directory)
             return
         btrfsutil.create_subvolume(btrfs_path + '/' + str(uuid1))
+        print('sudo cp -rf --reflink=auto ' + directory + '/* ' + btrfs_path + '/' + str(uuid))
         os.system('sudo cp -rf --reflink=auto ' + directory + '/* ' + btrfs_path + '/' + str(uuid))
         if not os.path.exists(btrfs_path + '/' + str(uuid1) + '/img.source'):
             file = open(btrfs_path + '/' + str(uuid1) + '/img.source', 'w')
