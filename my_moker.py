2946bb6d6b5a205a2f74497584e34e8b11e82f5a
NatVas <vas.nat.al@yandex.ru>
2019-12-16 02:23:07 +0300

    Update my_moker.py

--- my_moker.py
+++ my_moker.py
@@ -36,7 +36,7 @@ def init(directory):
             init(directory)
             return
         btrfsutil.create_subvolume(btrfs_path + '/' + str(uuid1))
-        os.system('cp -rf --reflink=auto ' + directory + '/* ' + btrfs_path + '/' + str(uuid))
+        os.system('sudo cp -rf --reflink=auto ' + directory + '/* ' + btrfs_path + '/' + str(uuid))
         if not os.path.exists(btrfs_path + '/' + str(uuid1) + '/img.source'):
             file = open(btrfs_path + '/' + str(uuid1) + '/img.source', 'w')
             file.write(directory)
